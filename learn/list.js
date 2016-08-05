0a847ac6a1bdf7dd330bdfac326c4790696871bd
LjjGit <ljjgit@qq.com>
2016-08-05 23:54:24 +0800

    用js实现了单页列表页

---
+++ learn/list.js
@@ -0,0 +1,12 @@
+function Allowing()
+{
+
+}
+Allowing.learn = {};
+Allowing.learn.list = [
+    {
+        title: 'PHP教程 - 准备篇',
+        createdAt: 1234567894,
+        page: '/learn/php-ready.md',
+    }
+];
