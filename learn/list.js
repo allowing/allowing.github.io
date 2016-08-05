949ddb63142076c33b43f9c8d44ee1379d9e1809
LjjGit <ljjgit@qq.com>
2016-08-06 00:11:59 +0800

    优化细节

--- learn/list.js
+++ learn/list.js
@@ -1,6 +1,5 @@
 function Allowing()
 {
-
 }
 Allowing.learn = {};
 Allowing.learn.list = [
@@ -8,5 +7,9 @@ Allowing.learn.list = [
         title: 'PHP教程 - 准备篇',
         createdAt: '2016/8/6',
         page: '/learn/php-ready.md',
+    }, {
+        title: 'PHP教程 - 基础篇',
+        createdAt: '2016/8/6',
+        page: '/learn/php-base.md',
     }
 ];
