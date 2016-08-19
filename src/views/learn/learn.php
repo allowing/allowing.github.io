cda8dee121d8defa5515a2cc412f2daa09f0ab00
weiwait <lijingwei.weiwait@formail.com>
2016-08-19 23:08:31 +0800

    优化

--- src/views/learn/learn.php
+++ src/views/learn/learn.php
@@ -9,7 +9,7 @@
 /** @var \allowing\yunliwang\model\MarkdownArticle $learn */
 $this->title = $learn->title;
 ?>
-
+<p class="tool-bar"><a href="javascript:history.back()">&lt;&lt;后退</a></p>
 <div class="item">
     <div class="item-content"></div>
 </div>
