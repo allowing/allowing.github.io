d350b9a5bb22f06f0508b4455bd915b7a2387e6d
allowing <ljjgit@qq.com>
2016-08-28 22:13:49 +0800

    fix bug

--- src/views/site/source.php
+++ src/views/site/source.php
@@ -4,11 +4,11 @@ $this->title = '学习资源';
 ?>
 
 <div class="item">
-    <h1 class="item-title">成功案例</h1>
+    <h1 class="item-title">学习资源</h1>
     <div class="item-content">
-        <p><a href="http://t.mb5u.com/html/">HTML 手册</a></p>
-        <p><a href="http://css.doyoe.com/">CSS 手册</a></p>
-        <p><a href="http://www.chinaw3c.org/standards.html">W3C 中国</a></p>
-        <p><a href="https://www.w3.org/">W3C 国际</a></p>
+        <p><a target="_blank" href="http://t.mb5u.com/html/">HTML 手册</a></p>
+        <p><a target="_blank" href="http://css.doyoe.com/">CSS 手册</a></p>
+        <p><a target="_blank" href="http://www.chinaw3c.org/standards.html">W3C 中国</a></p>
+        <p><a target="_blank" href="https://www.w3.org/">W3C 国际</a></p>
     </div>
 </div>
