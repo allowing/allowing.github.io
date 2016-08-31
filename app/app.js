5e8bc2b6e12fdd5f4166e10c396071a8c5c37b42
LjjGit <1076707907@qq.com>
2016-08-31 15:14:21 +0800

    清理掉练习文件

--- app/app.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-import React from 'react';
-import {render} from 'react-dom';
-import NewsList from './NewsList.js';
-import './app.css';
-
-render(<NewsList />, $('#content')[0]);
