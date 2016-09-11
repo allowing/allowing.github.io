dd6e42ce7195efe44eb8c2b00fac322c37f438c9
allowing <ljjgit@qq.com>
2016-09-11 22:11:51 +0800

    新增资源链接

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -13,16 +13,19 @@ use yii\helpers\Url;
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
+    <title><?= $this->title ?> - 允梨教育</title>
     <meta name="viewport" content="
         width=device-width,
         user-scalable=no,
         initial-scale=1.0,
         maximum-scale=1.0,
-        minimum-scale=1.0">
+        minimum-scale=1.0,
+        uc-fitscreen=no">
+    <meta name="layoutmode" content="standard">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $this->title ?> - 允梨教育</title>
     <meta name="keywords" content="<?= $this->title ?>,允梨,允梨教育,PHP自学,JS自学,PHP教程,IT,WEB,IT培训,HTML,JS,CSS,PHP">
     <meta name="description" content="允梨教育是一家学习IT技术的第三方培训平台，学习周期短，学习技能较多，从这里出来的学生均有较高的职业素养和较高的工资。想学习IT技术来允梨教育【允梨，用心做教育】">
+    <meta name="author" content="allowing">
     <link rel="stylesheet" href="<?= Url::to('@web/global.css') ?>">
     <link rel="stylesheet" href="<?= Url::to('@web/default.css') ?>">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/styles/monokai-sublime.min.css">
