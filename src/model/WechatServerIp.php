6403d3f8468fea84a96ad0e2ea697c68808a8930
allowing <ljjgit@qq.com>
2016-10-15 11:49:49 +0800

    修复微信服务器返回的 ip 格式错误导致的问题

--- src/model/WechatServerIp.php
+++ src/model/WechatServerIp.php
@@ -59,11 +59,41 @@ class WechatServerIp extends Model
 
         $this->_ipList = ArrayHelper::getValue($data, Key::IP_LIST);
 
+        // 修复 ip 格式的错误
+        $this->_ipList = $this->fixIpFormat($this->_ipList);
+
         return $this->_ipList;
     }
 
     public function isWechatServerIp($ip)
     {
         return ArrayHelper::isIn($ip, $this->getIpList());
+    }
+
+    /**
+     * 修复不符合格式的 ip
+     *
+     * 在获取到的微信服务器 ip ，发现有下列的格式
+     * ```
+     *  [90] => 101.226.103.0/25
+     *  [91] => 101.226.233.128/25
+     *  [92] => 58.247.206.128/25
+     *  [93] => 182.254.86.128/25
+     *  [94] => 103.7.30.21
+     *  [95] => 103.7.30.64/26
+     *  [96] => 58.251.80.32/27
+     *  [97] => 183.3.234.32/27
+     *  [98] => 121.51.130.64/27
+     * ```
+     * 这里要去掉如 /25 的字符
+     */
+    private function fixIpFormat(array $ipList)
+    {
+        return array_filter($ipList, function ($value) {
+            if (false !== $position = strpos($value, '/')) {
+                return substr($value, 0, $position);
+            }
+            return $value;
+        });
     }
 }
