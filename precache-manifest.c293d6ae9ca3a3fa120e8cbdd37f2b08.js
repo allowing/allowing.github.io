778e56ad11bd3e43e0474186cb106b36805a9b14
ljj <ljjgit@qq.com>
2018-10-14 03:12:51 +0800

    build

---
+++ precache-manifest.c293d6ae9ca3a3fa120e8cbdd37f2b08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "90950f78472f9a398462",
+    "url": "/static/js/main.90950f78.chunk.js"
+  },
+  {
+    "revision": "d54450c2eae91fd8693a",
+    "url": "/static/js/1.d54450c2.chunk.js"
+  },
+  {
+    "revision": "90950f78472f9a398462",
+    "url": "/static/css/main.acb36c87.chunk.css"
+  },
+  {
+    "revision": "a3dd29bf451a369ea341bddc671ebe25",
+    "url": "/index.html"
+  }
+];
